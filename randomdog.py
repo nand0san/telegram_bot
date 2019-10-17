578fd606322b28eeeb79576a87ab63bdb0683bea
unknown <ffad@DESKTOP-OFU7BM3.gmv.es>
2019-10-17 09:33:52 +0200

    primer commit con pruebas de fotos de perros

---
+++ randomdog.py
@@ -0,0 +1,26 @@
+
+from telegram.ext import Updater, CommandHandler
+# import urllib.request
+# import urllib.parse
+import requests
+import re
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('860310738:AAH540w4XJawuYAk9awMmlqF2lxyF5j5anM')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop', bop))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
